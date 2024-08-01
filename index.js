a6915dc4fd97a7d649ec0ce68340039b7d68d2e3
frank_h <1132990649@qq.com>
2024-08-02 00:28:03 +0800

    完善

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
-//- 上传图片，并在页面内显示该图片
 
-//- 支持 点击 和 拖拽 两种方式
+let dropArea = document.getElementById('dropArea');
+let fileNameArea = document.getElementById('fileName');
+let img = document.getElementById('img');
 
-//- 图片格式限制为 PNG
+//限制图片格式为png
+const isPng = (file) => {
+    return file.type === 'image/png';
+}
 
-let dropArea = document.getElementById('drop-area');
+//处理图片 转为base64
+const handleImg = (file) => {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function (e) {     
+        img.src = e.target.result;
+    }
+}
 
 //点击上传事件
 dropArea.addEventListener('click', function (e) {
     let input = document.createElement('input');
     input.type = 'file';
-    input.accept = 'image/png';
     input.click();
     input.addEventListener('change', function (e) {
         let file = e.target.files[0];
-        let reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = function (e) {
-            let img = document.getElementById('img');
-            img.src = e.target.result;
+        if(!isPng(file)){
+            alert('只能传入png');
+            return
         }
+        fileNameArea.innerText = file.name;
+        handleImg(file)
     });
+})
+
+//拖拽上传
+dropArea.addEventListener('dragover', function (e) {
+    e.preventDefault()
+ })
+ 
+dropArea.addEventListener('drop', function (e) {
+   e.preventDefault()
+    let file = e.dataTransfer.files[0]
+    if(!isPng(file)){
+        alert('只能传入png');
+        return
+    }
+    fileNameArea.innerText = file.name;
+    handleImg(file)
 })
